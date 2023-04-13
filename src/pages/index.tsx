88715ae926fb8d140ddf4265e8b5f31e3872e709
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-14 00:50:43 +0100

    bg relative

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../../components/Header";
 
 const Home: NextPage = () => {
 	return (
-		<div>
+		<div className='gradient-01 h-screen'>
 			<Head>
 				<title>E.i | frontend developer</title>
 			</Head>
