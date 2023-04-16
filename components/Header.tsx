5d192b9bc4692c95e02c9e3a2e8a02e808839657
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-16 10:47:02 +0100

    img

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,34 @@
 import React from "react";
+import logo from "../images/logo.png";
+
 import { SocialIcon } from "react-social-icons";
+import { type } from "os";
 
 type Props = {};
 
 export default function Header({}: Props) {
 	return (
 		<header className='top-5 items-center justify-between'>
-			<div className='flex flex-row items-center'>
+			<div className='flex items-center'>
 				{/* <SocialIcon
 					url='https://www.youtube.com/sonnysangha'
 					fgColor='black'
 					bgColor='transparent'
 				/> */}
+				{/* <img src={logo} alt='' /> */}
 			</div>
 
-			<nav className='mt-32 p-1 bg-red-800 border-emerald-950'>
+			<nav className='mt-32 bg-gray-800 border-emerald-950 p-16'>
 				<div className='flex'>
-					<ul className='flex w-full justify-between'>
-						<li className='text-white'>About</li>
-						<li className='text-white'>Projects</li>
-						<li className='text-white'>Blog</li>
-						<li className='text-white'>Contact Me</li>
+					<ul className='flex w-full justify-between text-3xl font-bold text-gray-400'>
+						<li className='pl-28'>About</li>
+						<li className=''>Projects</li>
+						<li className=''>Blog</li>
+						<li className='pr-28'>Contact Me</li>
 					</ul>
 				</div>
 			</nav>
+			<div className='bg-gray-700 h-1'></div>
 		</header>
 	);
 }
