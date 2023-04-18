569896fa29544fe230623c87f44c648663ba82e6
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-18 08:39:21 +0100

    __

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -6,11 +6,11 @@ type Props = {};
 function BackgroundCircles({}: Props) {
 	return (
 		<div className='relative flex justify-center items-center'>
-			<div className='absolute border [gray] rounded-full h-[200px] w-[200px] animate-ping' />
-			<div />
-			<div />
-			<div />
-			<div />
+			<div className='absolute border border-[indigo] rounded-full h-[200px] w-[200px] animate-ping' />
+			<div className='absolute border border-[indigo] rounded-full h-[300px] w-[300px] animate-ping' />
+			<div className='absolute border border-[indigo] rounded-full h-[500px] w-[500px] animate-ping' />
+			<div className='rounded-full border border-[gray] opacity-20 h-[650px] w-[650px]' />
+			<div className='absolute border border-[indigo] rounded-full h-[700px] w-[700px]' />
 		</div>
 	);
 }
