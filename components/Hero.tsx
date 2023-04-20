40a7736b904a47af9d446bd64393b76c743a28ab
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-20 14:40:20 +0100

    __

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 function Hero({}: Props) {
 	const [text, count] = useTypewriter({
-		words: ["hi", "I am Emmanuel Ingwe", ""],
+		words: ["hi,", "I am Emmanuel Ingwe", ""],
 		loop: true,
 		delaySpeed: 1400,
 	});
