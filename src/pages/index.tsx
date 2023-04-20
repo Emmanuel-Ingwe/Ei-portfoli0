dc1db71d18433d25a165309d717650643031df53
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-20 12:04:39 +0100

    title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
 	return (
 		<div className='gradient-01 h-screen text-gray-400 snap-y snap-madatory overflow-hidden z-0'>
 			<Head>
-				<title>E.i | frontend developer</title>
+				<title>E.i | Frontend Developer</title>
 			</Head>
 
 			<Header />
