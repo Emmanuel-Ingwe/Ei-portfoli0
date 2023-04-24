06b473d1ab67ce47cb54a8b0db08b43f1c084e00
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-24 23:56:50 +0100

    __

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 function index({}: Props) {
 	return (
-		<div className=''>
+		<div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 			<h3 className=''>About</h3>
 		</div>
 	);
