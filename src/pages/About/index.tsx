64ab76e989643bdf11e57874d0e6acf0359d6168
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-24 23:59:25 +0100

    img motion

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,7 +5,11 @@ type Props = {};
 function index({}: Props) {
 	return (
 		<div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-			<h3 className=''>About</h3>
+			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+				About
+			</h3>
+
+			<MotionConfig.img />
 		</div>
 	);
 }
