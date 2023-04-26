b02ea1c633cb7effee9895aa1a91760b90b459b7
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-26 23:23:38 +0100

    +_

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,10 +5,10 @@ type Props = {};
 
 function index({}: Props) {
 	return (
-		<div>
-			<Header />
+		<div className='gradient-01'>
+			{/* <Header /> */}
 
-			<div className='gradient-01 overflow-hidden flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+			<div className='overflow-hidden flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 				<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
 					About
 				</h3>
