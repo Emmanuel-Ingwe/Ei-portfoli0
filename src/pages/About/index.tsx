2155419c63ba85d47d7dc88f270a7c0f5c21a06a
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-04-29 18:08:50 +0100

    styles

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -14,11 +14,10 @@ function index({}: Props) {
 
 			<Header />
 
-			<div className='overflow-hidde flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+			<div className='container overflow-hidde flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 				<h3 className='absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl'>
 					About
 				</h3>
-
 				<motion.img
 					initial={{
 						x: -200,
