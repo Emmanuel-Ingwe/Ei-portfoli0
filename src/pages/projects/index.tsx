8b16ebe811ad7f96df74b40e13aaed6f9d1adddd
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-03 12:03:02 +0100

    exp

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Header from "../Header";
 
 type Props = {};
 
@@ -10,7 +11,11 @@ function index({}: Props) {
 				<title>E.i | my Projects</title>
 			</Head>
 
-			<div className='gradient-01 h-screen text-gray-400 snap-y snap-madatory overflow-hidden z-0'></div>
+			<Header />
+
+			<div className='gradient-01 h-screen text-gray-400 snap-y snap-madatory overflow-hidden z-0'>
+				<h3 className=''>Experience</h3>
+			</div>
 		</div>
 	);
 }
