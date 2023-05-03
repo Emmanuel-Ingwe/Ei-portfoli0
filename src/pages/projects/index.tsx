5d54c7cc1ce17cdf0e50b4a36ebebec8f2d2ad58
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-03 17:00:28 +0100

    img

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,15 +6,25 @@ type Props = {};
 
 function index({}: Props) {
 	return (
-		<div className='gradient-01 h-full w-full text-gray-400 snap-y snap-madatory overflow-hidden z-0'>
+		<div className='gradient-01 h-screen w-full text-gray-400 snap-y snap-madatory overflow-hidden z-0'>
 			<Head>
 				<title>E.i | my Projects</title>
 			</Head>
 
 			<Header />
 
-			<div className='container'>
-				<h3 className=''>Experience</h3>
+			<div className='container flex flex-col items-center justify-center'>
+				<h3 className='uppercase tracking-[20px] text-gray-500 text-2xl'>
+					Projects
+				</h3>
+
+				<article>
+					<img
+						src='disney-big.jpg'
+						alt=''
+						className='w-32 h-32 rounded-lg xl:w-[200px] object-cover object-center'
+					/>
+				</article>
 			</div>
 		</div>
 	);
