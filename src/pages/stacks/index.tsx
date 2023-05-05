8a638267ca7c5ad438cbc5f584339cd7730a253f
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-05 15:17:18 +0100

    skill.h3_motion

--- src/pages/stacks/index.tsx
+++ src/pages/stacks/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Head from "next/head";
+import Header from "../Header";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function index({}: Props) {
-	return <div>index</div>;
+	return (
+		<div className='gradient-01 h-screen w-full text-gray-400 snap-y snap-madatory overflow-hidde z-0'>
+			<Head>
+				<title>E.i | my Projects</title>
+			</Head>
+
+			<Header />
+			<div className='container flex flex-col items-center justify-center'>
+				<motion.h3
+					initial={{
+						y: -100,
+						opacity: 0,
+					}}
+					transition={{
+						duration: 1.2,
+					}}
+					whileInView={{ opacity: 1, y: 0 }}
+					viewport={{ once: true }}
+					className='uppercase tracking-[20px] text-gray-500 text-2xl m-10'>
+					Skills
+				</motion.h3>
+			</div>
+		</div>
+	);
 }
 
 export default index;
