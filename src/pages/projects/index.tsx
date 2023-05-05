5829506d4409d41fba0229a3f9fd9ec5d736ae89
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-05 13:01:33 +0100

    _rnded

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -19,7 +19,7 @@ function index({}: Props) {
 					Projects
 				</h3>
 
-				<div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+				<div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory mb-20'>
 					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
@@ -44,7 +44,7 @@ function index({}: Props) {
 							</p>
 						</div>
 					</article>
-					<article className='flex flex-col items-center justify-center  space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335]'>
+					<article className='flex flex-col items-center justify-center  space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
 								y: -100,
@@ -68,7 +68,7 @@ function index({}: Props) {
 							</p>
 						</div>
 					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335]'>
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
 								y: -100,
@@ -92,7 +92,7 @@ function index({}: Props) {
 							</p>
 						</div>
 					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335]'>
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
 								y: -100,
@@ -116,7 +116,7 @@ function index({}: Props) {
 							</p>
 						</div>
 					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335]'>
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
 								y: -100,
@@ -140,7 +140,7 @@ function index({}: Props) {
 							</p>
 						</div>
 					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335]'>
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 						<motion.img
 							initial={{
 								y: -100,
