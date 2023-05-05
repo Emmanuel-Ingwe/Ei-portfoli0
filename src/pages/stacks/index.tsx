3930fccd0e1e4f6685887d9ab0a5f453550d02f0
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-05 16:38:35 +0100

    __

--- src/pages/stacks/index.tsx
+++ src/pages/stacks/index.tsx
@@ -3,16 +3,19 @@ import Head from "next/head";
 import Header from "../Header";
 import { motion } from "framer-motion";
 
-type Props = {};
+type Props = {
+	directionLeft?: boolean;
+};
 
-function index({}: Props) {
+function stacks({ directionLeft }: Props) {
 	return (
-		<div className='gradient-01 h-screen w-full text-gray-400 snap-y snap-madatory overflow-hidde z-0'>
+		<div className='gradient-01 h-full w-full text-gray-400 snap-y snap-madatory overflow-hidde z-0'>
 			<Head>
 				<title>E.i | my Projects</title>
 			</Head>
 
 			<Header />
+
 			<div className='container flex flex-col items-center justify-center'>
 				<motion.h3
 					initial={{
@@ -27,9 +30,192 @@ function index({}: Props) {
 					className='uppercase tracking-[20px] text-gray-500 text-2xl m-10'>
 					Skills
 				</motion.h3>
+
+				<div className='flex flex-wrap items-center justify-center w-2/4 '>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer p-3'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+					<div className='group relative flex cursor-pointer'>
+						<motion.img
+							initial={{
+								x: directionLeft ? -200 : 200,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 1,
+							}}
+							whileInView={{ opacity: 1, x: 0 }}
+							src='tailwind.jpg'
+							alt=''
+							className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale'
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 }
 
-export default index;
+export default stacks;
