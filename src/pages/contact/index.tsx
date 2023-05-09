412986d7e8fbb0436f51e2748cdd9f2dcdf52dbb
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-09 11:57:14 +0100

    __

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -17,14 +17,14 @@ function contact({ directionLeft }: Props) {
 
 			<Header />
 
-			<div className='containr flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+			<div className='container flex relative flex-col text-center max-w-7xl px-10 justify-evenly mx-auto items-center'>
 				<h3 className='tracking-[20px] uppercase text-gray-500 text-2xl m-10'>
 					Contact
 				</h3>
 
-				<div className='flex flex-col space-y-10'>
-					<h4 className='text-2xl fontsemibold text-center'>
-						I got what you looking for.{""}{" "}
+				<div className='flex flex-col space-y-10 m-10'>
+					<h4 className='text-1xl font-semibold text-center md:text-2xl'>
+						I got what you looking for.{""} {""}
 						<span className='decoration-red-900 underline'>Let's talk</span>
 					</h4>
 
@@ -43,16 +43,28 @@ function contact({ directionLeft }: Props) {
 						</div>
 					</div>
 
-					<form className='flex flex-col' action=''>
-						<div>
-							<input type='text' />
-							<input type='text' />
+					<form className='flex flex-col space-y-2 w-fit mx-auto' action=''>
+						<div className='flex space-x-2'>
+							<input placeholder='Name' className='contactInput' type='text' />
+							<input
+								placeholder='Email'
+								className='contactInput'
+								type='email'
+							/>
 						</div>
 
-						<input type='text' />
+						<input placeholder='Subject' className='contactInput' type='text' />
 
-						<textarea name='' id=''></textarea>
-						<button className=''>Submit</button>
+						<textarea
+							placeholder='Message'
+							className='contactInput'
+							name=''
+							id=''></textarea>
+						<button
+							type='submit'
+							className='bg-red-950 py-5 px-10 rounded-md text-black font-bold text-lg'>
+							Submit
+						</button>
 					</form>
 				</div>
 			</div>
