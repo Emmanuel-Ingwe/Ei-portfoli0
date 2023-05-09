a340edacb2f78b431848df0c2cc87bae02a33a87
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-09 09:35:47 +0100

    input styles

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -28,7 +28,32 @@ function contact({ directionLeft }: Props) {
 						<span className='decoration-red-900 underline'>Let's talk</span>
 					</h4>
 
-					<div className=''></div>
+					<div className='space-y-10'>
+						<div className='flex items-center space-x-5 justify-center'>
+							<PhoneIcon className='text-red-900 h-7 w-7 animate-pulse' />
+							<p className='text-1xl'>+2348139320656</p>
+						</div>
+						<div className='flex items-center space-x-5 justify-center'>
+							<EnvelopeIcon className='text-red-900 h-7 w-7 animate-pulse' />
+							<p className='text-1xl'>ingwemmanuel@gmail.com</p>
+						</div>
+						<div className='flex items-center space-x-5 justify-center'>
+							<MapPinIcon className='text-red-900 h-7 w-7 animate-pulse' />
+							<p className='text-1xl'>Calabar, CRS</p>
+						</div>
+					</div>
+
+					<form className='flex flex-col' action=''>
+						<div>
+							<input type='text' />
+							<input type='text' />
+						</div>
+
+						<input type='text' />
+
+						<textarea name='' id=''></textarea>
+						<button className=''>Submit</button>
+					</form>
 				</div>
 			</div>
 		</div>
