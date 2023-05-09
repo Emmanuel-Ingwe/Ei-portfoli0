857a4bcd09885d4029dd78d622bb7b1473592565
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-09 16:39:20 +0100

    imports del.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Head from "next/head";
 import React from "react";
 import Header from "./Header";
 import Hero from "../../components/Hero";
-import SocialIcons from "../../components/SocialIcons";
-import Mailto from "../../components/Mailto";
 
 const Home: NextPage = () => {
 	return (
