85a1d7164027fa0440a8782a78971b306c1ac453
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-15 16:28:43 +0100

    _

--- src/pages/stacks/index.tsx
+++ src/pages/stacks/index.tsx
@@ -211,7 +211,7 @@ function stacks({ directionLeft }: Props) {
 								whileInView={{ opacity: 1, x: 0 }}
 								src='ts.jpg'
 								alt=''
-								className='rounded-full border border-gray-500  object-cover w-24 h-24 xl:w-32 xl:h-32 filter grayscale group-hover:grayscale-0 transition duration-300 ease-in-out'
+								className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter grayscale group-hover:grayscale-0 transition duration-300 ease-in-out'
 							/>
 						</div>
 					</div>
