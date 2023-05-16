fd8096f0b41b6c6b82327dded47ce628bb233ec4
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-05-16 07:36:36 +0100

    _

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -23,126 +23,127 @@ function contact({}: Props) {
 	};
 
 	return (
-		<div className='gradient-01  h-full md:h-screen w-full text-gray-400 snap-y snap-madatory overflow-x-hidden  overflow-y-scroll scrollbar-track-gray-700/20 scrollbar-thumb-red-950 scrollbar-thin z-0'>
-			<Head>
-				<title>E.i | my Projects</title>
-			</Head>
+		<div>
+			<div className='gradient-01  h-full md:h-screen w-full text-gray-400 snap-y snap-madatory overflow-x-hidden  overflow-y-scroll scrollbar-track-gray-700/20 scrollbar-thumb-red-950 scrollbar-thin z-0'>
+				<Head>
+					<title>E.i | my Projects</title>
+				</Head>
 
-			<Header />
+				<Header />
 
-			<div className='container flex relative flex-col text-center max-w-7xl px-10 justify-evenly mx-auto items-center'>
-				<h3 className='tracking-[20px] uppercase text-gray-500 text-2xl m-10'>
-					Contact
-				</h3>
+				<div className='container flex relative flex-col text-center max-w-7xl px-10 justify-evenly mx-auto items-center'>
+					<h3 className='tracking-[20px] uppercase text-gray-500 text-2xl m-10'>
+						Contact
+					</h3>
 
-				<div className='flex flex-col space-y-10 m-10'>
-					<h4 className='text-1xl font-semibold text-center md:text-2xl'>
-						I got what you looking for.{""} {""}
-						<span className='decoration-red-900 underline'>Let's talk</span>
-					</h4>
+					<div className='flex flex-col space-y-10 m-10'>
+						<h4 className='text-1xl font-semibold text-center md:text-2xl'>
+							I got what you looking for.{""} {""}
+							<span className='decoration-red-900 underline'>Let's talk</span>
+						</h4>
 
-					<div className='space-y-10'>
-						<div className='flex items-center space-x-5 justify-center'>
-							<PhoneIcon className='text-red-900 h-7 w-7 animate-pulse' />
-							<p className='text-1xl animate-bounce'>
-								<a href='https://wa.me/message/FI6OFAJRBKVMI1'>
-									+2348139320656{" "}
-								</a>
-							</p>
+						<div className='space-y-10'>
+							<div className='flex items-center space-x-5 justify-center'>
+								<PhoneIcon className='text-red-900 h-7 w-7 animate-pulse' />
+								<p className='text-1xl animate-bounce'>
+									<a href='https://wa.me/message/FI6OFAJRBKVMI1'>
+										+2348139320656{" "}
+									</a>
+								</p>
+							</div>
+							<div className='flex items-center space-x-5 justify-center'>
+								<EnvelopeIcon className='text-red-900 h-7 w-7 animate-pulse' />
+								<p className='text-1xl'>ingwemmanuel@gmail.com</p>
+							</div>
+							<div className='flex items-center space-x-5 justify-center'>
+								<MapPinIcon className='text-red-900 h-7 w-7 animate-pulse' />
+								<p className='text-1xl'>Calabar, CRS</p>
+							</div>
 						</div>
-						<div className='flex items-center space-x-5 justify-center'>
-							<EnvelopeIcon className='text-red-900 h-7 w-7 animate-pulse' />
-							<p className='text-1xl'>ingwemmanuel@gmail.com</p>
-						</div>
-						<div className='flex items-center space-x-5 justify-center'>
-							<MapPinIcon className='text-red-900 h-7 w-7 animate-pulse' />
-							<p className='text-1xl'>Calabar, CRS</p>
-						</div>
-					</div>
 
-					<form
-						onSubmit={handleSubmit(onSubmit)}
-						className='flex flex-col space-y-2 w-screen sm:w-fit p-3'
-						action=''>
-						<div className='flex flex-col sm:flex-row sm:space-x-2'>
+						<form
+							onSubmit={handleSubmit(onSubmit)}
+							className='flex flex-col space-y-2 w-screen sm:w-fit p-3'
+							action=''>
+							<div className='flex flex-col sm:flex-row sm:space-x-2'>
+								<motion.input
+									{...register("name")}
+									initial={{
+										y: 300,
+									}}
+									transition={{
+										duration: 0.3,
+									}}
+									whileInView={{ y: 0 }}
+									viewport={{ once: true }}
+									placeholder='Name'
+									className='contactInput mb-1'
+									type='text'
+								/>
+								<motion.input
+									{...register("email")}
+									initial={{
+										y: 300,
+									}}
+									transition={{
+										duration: 0.3,
+									}}
+									whileInView={{ y: 0 }}
+									viewport={{ once: false }}
+									placeholder='Email'
+									className='contactInput mb-1'
+									type='email'
+								/>
+							</div>
+
 							<motion.input
-								{...register("name")}
+								{...register("subject")}
 								initial={{
 									y: 300,
 								}}
 								transition={{
-									duration: 0.3,
-								}}
-								whileInView={{ y: 0 }}
-								viewport={{ once: true }}
-								placeholder='Name'
-								className='contactInput mb-1'
-								type='text'
-							/>
-							<motion.input
-								{...register("email")}
-								initial={{
-									y: 300,
-								}}
-								transition={{
-									duration: 0.3,
+									duration: 0.4,
 								}}
 								whileInView={{ y: 0 }}
 								viewport={{ once: false }}
-								placeholder='Email'
-								className='contactInput mb-1'
-								type='email'
+								placeholder='Subject'
+								className='contactInput'
+								type='text'
 							/>
-						</div>
 
-						<motion.input
-							{...register("subject")}
-							initial={{
-								y: 300,
-							}}
-							transition={{
-								duration: 0.4,
-							}}
-							whileInView={{ y: 0 }}
-							viewport={{ once: false }}
-							placeholder='Subject'
-							className='contactInput'
-							type='text'
-						/>
+							<motion.textarea
+								{...register("message")}
+								initial={{
+									y: 240,
+								}}
+								transition={{
+									duration: 0.5,
+								}}
+								whileInView={{ y: 0 }}
+								viewport={{ once: false }}
+								placeholder='Message'
+								className='contactInput'
+								typeof='text'
+								name='message'
+								id=''></motion.textarea>
 
-						<motion.textarea
-							{...register("message")}
-							initial={{
-								y: 240,
-							}}
-							transition={{
-								duration: 0.5,
-							}}
-							whileInView={{ y: 0 }}
-							viewport={{ once: false }}
-							placeholder='Message'
-							className='contactInput'
-							typeof='text'
-							name='message'
-							id=''></motion.textarea>
-
-						<motion.button
-							initial={{
-								x: 250,
-							}}
-							transition={{
-								duration: 0.55,
-							}}
-							whileInView={{ x: 0 }}
-							viewport={{ once: false }}
-							type='submit'
-							className='bg-red-950 py-5 px-10 rounded-md text-black font-bold text-lg'>
-							Submit
-						</motion.button>
-					</form>
+							<motion.button
+								initial={{
+									x: 250,
+								}}
+								transition={{
+									duration: 0.55,
+								}}
+								whileInView={{ x: 0 }}
+								viewport={{ once: false }}
+								type='submit'
+								className='bg-red-950 py-5 px-10 rounded-md text-black font-bold text-lg'>
+								Submit
+							</motion.button>
+						</form>
+					</div>
 				</div>
 			</div>
-
 			<Footer />
 		</div>
 	);
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -8,163 +8,165 @@ type Props = {};
 
 function index({}: Props) {
 	return (
-		<div className='gradient-01 h-full md:h-screen xl:h-full w-full text-gray-400 snap-y snap-madatory overflow-y-scroll scrollbar-thin scrollbar-track-gray-700/20 scrollbar-thumb-red-950vfbtgnt z-0'>
-			<Head>
-				<title>E.i | my Projects</title>
-			</Head>
+		<div>
+			<div className='gradient-01 h-full md:h-screen xl:h-full w-full text-gray-400 snap-y snap-madatory overflow-y-scroll scrollbar-thin scrollbar-track-gray-700/20 scrollbar-thumb-red-950vfbtgnt z-0'>
+				<Head>
+					<title>E.i | my Projects</title>
+				</Head>
 
-			<Header />
+				<Header />
 
-			<div className='container flex flex-col items-center justify-center'>
-				<h3 className='uppercase tracking-[20px] text-gray-500 text-2xl m-10'>
-					Projects
-				</h3>
+				<div className='container flex flex-col items-center justify-center'>
+					<h3 className='uppercase tracking-[20px] text-gray-500 text-2xl m-10'>
+						Projects
+					</h3>
 
-				<div className='w-full flex space-x-5 overflow-x-scroll  scrollbar-track-gray-700/20 scrollbar-thumb-red-950 scrollbar-thin p-10 snap-x snap-mandatory mb-20'>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='disney-big.jpg'
-							alt=''
-							className='h-52 w-64 md:w-96 xl:w-3/5 rounded-2xl object-cover object-center'
-						/>
+					<div className='w-full flex space-x-5 overflow-x-scroll  scrollbar-track-gray-700/20 scrollbar-thumb-red-950 scrollbar-thin p-10 snap-x snap-mandatory mb-20'>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 md:w-96 xl:w-3/5 rounded-2xl object-cover object-center'
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
-					<article className='flex flex-col items-center justify-center  space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='hulu-big.jpg'
-							alt=''
-							className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
-						/>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Disney Clone</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center  space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='hulu-big.jpg'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='disney-big.jpg'
-							alt=''
-							className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
-						/>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Disney Clone</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='disney-big.jpg'
-							alt=''
-							className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center '
-						/>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Disney Clone</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center '
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='disney-big.jpg'
-							alt=''
-							className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
-						/>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Disney Clone</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
-					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{
-								duration: 1.2,
-							}}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='disney-big.jpg'
-							alt=''
-							className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
-						/>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Case study:</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
+							/>
 
-						<div className='px-0 md:px-10'>
-							<h4 className='text-3xl font-light'>Disney Clone</h4>
-							<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Quibusdam, qui.
-							</p>
-						</div>
-					</article>
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>Disney Clone</h4>
+								<p className='text-gray-500 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Quibusdam, qui.
+								</p>
+							</div>
+						</article>
+					</div>
 				</div>
 			</div>
 			<Footer />
