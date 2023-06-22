89c4696129ce5ec073cc70aaadc30b22bd111a2e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-22 12:38:19 +0100

    hu clone

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -44,18 +44,135 @@ function index({}: Props) {
 									Case study 1{" "}
 									<small className='font-thin text-gray-500'>of 1 :</small>{" "}
 									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-<<<<<<< HEAD
 										<a href='https://disney-clone-gilt.vercel.app/'>Disney Clone</a>
-=======
-										<a href='https://github.com/Emmanuel-ingwe/disney-clone'>Disney Clone</a>
->>>>>>> 7980361 (dis clone)
 									</small>
 								</h4>
 								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
 									Disney clone web app, built with nextjs, tailwind, firebase
 									and google auth.
 								</p>
-								<GitIcon />
+								<GitIcon link="https://github.com/Emmanuel-ingwe/disney-clone" />
+							</div>
+						</article>
+
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='kick-nation.png'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 3 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://kick-nationfigm.vercel.app/'>Kick Nation</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+									A Mystery football shirt experience store app. built with nextjs and tailwind.
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-Ingwe/kick-nationfigm" />
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='kick-nation.png'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center '
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 4 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://github.com/Emmanuel-ingwe'>----</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+									Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-ingwe" />
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='mod-webs.png'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 5 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://bank-modern-app-rust-alpha.vercel.app/'>Payment website</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+										A Modern website Ui, Built with react and tailwind css.
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-Ingwe/bank-modern-app" />
+							</div>
+						</article>
+						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='e-com.png'
+								alt=''
+								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 6 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://github.com/Emmanuel-ingwe'>Flutterwave Clone.</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+									Fluttewave website clone, built with html,css & javascript.
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-Ingwe/Flutterwave_clone" />
 							</div>
 						</article>
 
@@ -86,134 +203,7 @@ function index({}: Props) {
 								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
 									Hulu clone web app, built with react, tailwind and firebase
 								</p>
-<<<<<<< HEAD
 								<GitIcon link='https://github.com/Emmanuel-Ingwe/hulu-2-0' />
-=======
-								<GitIcon link='' />
-							</div>
-						</article>
-						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px]  snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-							<motion.img
-								initial={{
-									y: -100,
-									opacity: 0,
-								}}
-								transition={{
-									duration: 1.2,
-								}}
-								whileInView={{ opacity: 1, y: 0 }}
-								viewport={{ once: true }}
-								src='disney-big.jpg'
-								alt=''
-								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
-							/>
-
-							<div className='px-0 md:px-10'>
-								<h4 className='text-3xl font-light'>
-									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 3 :</small>{" "}
-									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-										<a href='https://github.com/Emmanuel-ingwe'>Hulu Clone</a>
-									</small>
-								</h4>
-								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
-									Disney clone web app, built with nextjs, tailwind and google
-									auth.
-								</p>
-								<GitIcon link="https://github.com/Emmanuel-ingwe" />
-							</div>
-						</article>
-						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-							<motion.img
-								initial={{
-									y: -100,
-									opacity: 0,
-								}}
-								transition={{
-									duration: 1.2,
-								}}
-								whileInView={{ opacity: 1, y: 0 }}
-								viewport={{ once: true }}
-								src='disney-big.jpg'
-								alt=''
-								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center '
-							/>
-
-							<div className='px-0 md:px-10'>
-								<h4 className='text-3xl font-light'>
-									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 4 :</small>{" "}
-									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-										<a href='https://github.com/Emmanuel-ingwe'>Hulu Clone</a>
-									</small>
-								</h4>
-								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
-									Disney clone web app, built with nextjs, tailwind and google
-									auth.
-								</p>
-								<GitIcon link="https://github.com/Emmanuel-ingwe" />
-							</div>
-						</article>
-						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-							<motion.img
-								initial={{
-									y: -100,
-									opacity: 0,
-								}}
-								transition={{
-									duration: 1.2,
-								}}
-								whileInView={{ opacity: 1, y: 0 }}
-								viewport={{ once: true }}
-								src='disney-big.jpg'
-								alt=''
-								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center items-center justify-center'
-							/>
-
-							<div className='px-0 md:px-10'>
-								<h4 className='text-3xl font-light'>
-									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 5 :</small>{" "}
-									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-										<a href='https://github.com/Emmanuel-ingwe'>Hulu Clone</a>
-									</small>
-								</h4>
-								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
-									Disney clone web app, built with nextjs, tailwind and google
-									auth.
-								</p>
-								<GitIcon link="https://github.com/Emmanuel-ingwe" />
-							</div>
-						</article>
-						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-							<motion.img
-								initial={{
-									y: -100,
-									opacity: 0,
-								}}
-								transition={{
-									duration: 1.2,
-								}}
-								whileInView={{ opacity: 1, y: 0 }}
-								viewport={{ once: true }}
-								src='disney-big.jpg'
-								alt=''
-								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
-							/>
-
-							<div className='px-0 md:px-10'>
-								<h4 className='text-3xl font-light'>
-									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 6 :</small>{" "}
-									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-										<a href='https://github.com/Emmanuel-ingwe'>Hulu Clone</a>
-									</small>
-								</h4>
-								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
-									Disney clone web app, built with nextjs, tailwind and google
-									auth.
-								</p>
-								<GitIcon />
 							</div>
 						</article>
 					</div>
