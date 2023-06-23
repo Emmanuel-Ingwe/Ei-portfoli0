ee300ddf5d72ede15d02633b02009d2f7fe8f0fd
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-23 21:03:43 +0100

    _

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -46,15 +46,7 @@ function contact({}: Props) {
 							<div className='flex items-center space-x-5 justify-center'>
 								<PhoneIcon className='text-red-900 h-7 w-7 animate-pulse' />
 								<p className='text-1xl animate-bounce'>
-<<<<<<< HEAD
-<<<<<<< HEAD
 									<a href='https://wa.me/message/5CHBOGLFKMBVB1'>
-=======
-									<a href='https://wa.me'>
->>>>>>> 828f7fd (wa/me)
-=======
-									<a href='https://wa.me/message/5CHBOGLFKMBVB1'>
->>>>>>> 0be6a0b (latest commit)
 										+2348139320656{" "}
 									</a>
 								</p>
