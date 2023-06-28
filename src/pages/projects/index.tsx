b1c88f24056f5726b98c3c1a7a20f1f615501550
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-28 18:43:59 +0100

    portfo proj link

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -112,7 +112,7 @@ function index({}: Props) {
 								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
 									Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
 								</p>
-								<GitIcon link="https://github.com/Emmanuel-ingwe" />
+								<GitIcon link="https://github.com/Emmanuel-Ingwe/Ei-portfoli0" />
 							</div>
 						</article>
 						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
