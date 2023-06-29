4af2995940de875682fcfd37c9956aedc802fb45
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-29 22:34:03 +0100

    _

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -23,6 +23,38 @@ function index({}: Props) {
 					</h3>
 
 					<div className='w-full flex space-x-5 overflow-x-scroll  scrollbar-track-gray-700/20 scrollbar-thumb-red-950 scrollbar-thin p-10 snap-x snap-mandatory mb-20'>
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
+								src='disney-big.jpg'
+								alt=''
+								className='h-52 w-64 md:w-96 xl:w-3/5 rounded-2xl object-cover object-center'
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 1 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://disney-clone-yujs.vercel.app/'>Meta website</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+									Disney clone web app, built with nextjs, tailwind, firebase
+									and google auth.
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-ingwe/disney-clone" />
+							</div>
+						</article>
+
 						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
 							<motion.img
 								initial={{
