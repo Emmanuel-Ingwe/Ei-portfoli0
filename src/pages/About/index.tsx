32fa4200a2689c3a9f7eab64c3f2b20ead264742
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-01 06:32:17 +0100

    fixed md query smaller scrn.

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -16,7 +16,7 @@ function index({}: Props) {
 			<Header />
 
 			<div className='about container overflow-hidden flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-				<h3 className='absolute top-6 uppercase tracking-[20px] text-gray-500 text-2xl'>
+				<h3 className='about-h3 absolute top-6 uppercase tracking-[20px] text-gray-500 text-2xl'>
 					About
 				</h3>
 				<motion.img
@@ -30,7 +30,7 @@ function index({}: Props) {
 					viewport={{ once: true }}
 					src='about-img.jpg'
 					// width='380px'
-					className='top-40 rounded-[20%] -mb-32 w-60 h-64 md:mb-0 flex-shrink-0 object-cover md:w-[300px] md:h-96 xl:w-[340px] xl:h-[440px] xl:rounded-2xl sm:w-[200px] sm:h-[200px]'
+					className='about-img top-40 rounded-[20%] -mb-32 w-60 h-64 md:mb-0 flex-shrink-0 object-cover md:w-[300px] md:h-96 xl:w-[340px] xl:h-[440px] xl:rounded-2xl sm:w-[200px] sm:h-[200px]'
 				/>
 				<div className='space-y-10 px-0 md:px-10 pt-20'>
 					<h4 className='text-3xl font text-red-900 sm:text-4xl'>
