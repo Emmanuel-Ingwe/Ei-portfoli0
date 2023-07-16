f9d0dbcfdd7c92fbac2c237698120ee41277d100
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-16 07:56:46 +0100

    imgCarshow

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -34,6 +34,37 @@ function Projects({}: Props) {
 								}}
 								whileInView={{ opacity: 1, y: 0 }}
 								viewport={{ once: true }}
+								src='CarShow.png'
+								alt=''
+								className='h-52 w-64 md:w-96 xl:w-3/5 rounded-2xl object-cover object-center'
+							/>
+
+							<div className='px-0 md:px-10'>
+								<h4 className='text-3xl font-light'>
+									Case study 1{" "}
+									<small className='font-thin text-gray-500'>of 1 :</small>{" "}
+									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
+										<a href='https://next-meta-webst-3nlv.vercel.app/'>Meta website</a>
+									</small>
+								</h4>
+								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
+									Meta website, built with react, nextjs, tailwind, and framer motion
+								</p>
+								<GitIcon link="https://github.com/Emmanuel-Ingwe/next-meta-webst" />
+							</div>
+						</article>
+						
+					<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
+							<motion.img
+								initial={{
+									y: -100,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 1.2,
+								}}
+								whileInView={{ opacity: 1, y: 0 }}
+								viewport={{ once: true }}
 								src='meta.png'
 								alt=''
 								className='h-52 w-64 md:w-96 xl:w-3/5 rounded-2xl object-cover object-center'
