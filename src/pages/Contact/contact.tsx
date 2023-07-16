22be0bc9d1de094d74c8c1826eea34b7cb61b917
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-07-16 08:43:06 +0100

    //

--- src/pages/Contact/contact.tsx
+++ src/pages/Contact/contact.tsx
@@ -15,7 +15,7 @@ type Inputs = {
 
 type Props = {};
 
-function contact({}: Props) {
+function index({}: Props) {
 	const { register, handleSubmit } = useForm<Inputs>();
 
 	const onSubmit: SubmitHandler<Inputs> = (formData) => {
@@ -149,4 +149,4 @@ function contact({}: Props) {
 	);
 }
 
-export default contact;
+export default index;
