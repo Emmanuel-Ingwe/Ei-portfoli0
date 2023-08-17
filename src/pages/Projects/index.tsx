4d2704fb416d85cf9307c48e8ca45d1772fffb18
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-08-17 02:46:34 +0100

    rem: old proj

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -135,7 +135,7 @@ function Projects({}: Props) {
 							<div className='px-0 md:px-10'>
 								<h4 className='text-3xl font-light'>
 									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 2 :</small>{" "}
+									<small className='font-thin text-gray-500'>of 4 :</small>{" "}
 									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
 										<a href='https://next-meta-webst-3nlv.vercel.app/'>Meta website</a>
 									</small>
@@ -166,7 +166,7 @@ function Projects({}: Props) {
 							<div className='px-0 md:px-10'>
 								<h4 className='text-3xl font-light'>
 									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 3 :</small>{" "}
+									<small className='font-thin text-gray-500'>of 5 :</small>{" "}
 									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
 										<a href='https://disney-clone-yujs.vercel.app/'>Disney Clone</a>
 									</small>
@@ -198,7 +198,7 @@ function Projects({}: Props) {
 							<div className='px-0 md:px-10'>
 								<h4 className='text-3xl font-light'>
 									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 4 :</small>{" "}
+									<small className='font-thin text-gray-500'>of 6 :</small>{" "}
 									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
 										<a href='https://kick-nationfigm.vercel.app/'>Kick Nation</a>
 									</small>
@@ -258,7 +258,7 @@ function Projects({}: Props) {
 							<div className='px-0 md:px-10'>
 								<h4 className='text-3xl font-light'>
 									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 6 :</small>{" "}
+									<small className='font-thin text-gray-500'>of 7 :</small>{" "}
 									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
 										<a href='https://bank-modern-app-rust-alpha.vercel.app/'>Payment website</a>
 									</small>
@@ -267,36 +267,6 @@ function Projects({}: Props) {
 										A Modern website Ui, Built with react and tailwind css.
 								</p>
 								<GitIcon link="https://github.com/Emmanuel-Ingwe/bank-modern-app" />
-							</div>
-						</article>
-						<article className='flex flex-col items-center justify-center space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#161335] p-10 hover:opacity-100 opacity-40 cursor-pointed transition-opacity duration-200 overflow-hidden'>
-							<motion.img
-								initial={{
-									y: -100,
-									opacity: 0,
-								}}
-								transition={{
-									duration: 1.2,
-								}}
-								whileInView={{ opacity: 1, y: 0 }}
-								viewport={{ once: true }}
-								src='e-com.png'
-								alt=''
-								className='h-52 w-64 rounded-2xl md:w-96 xl:w-3/5 object-cover object-center'
-							/>
-
-							<div className='px-0 md:px-10'>
-								<h4 className='text-3xl font-light'>
-									Case study 1{" "}
-									<small className='font-thin text-gray-500'>of 7 :</small>{" "}
-									<small className='font-thin text-cyan-900 hover:text-cyan-700 transition duration-500'>
-										<a href='https://github.com/Emmanuel-ingwe'>Flutterwave Clone.</a>
-									</small>
-								</h4>
-								<p className=' text-gray-300 underline decoration-red-950 text-1xl mt-1'>
-									Fluttewave website clone, built with html,css & javascript.
-								</p>
-								<GitIcon link="https://github.com/Emmanuel-Ingwe/Flutterwave_clone" />
 							</div>
 						</article>
 
