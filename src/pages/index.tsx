7c7b41ee7abf3abe0b037d7a44c6bffda5809d47
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-07-18 16:12:06 +0100

    add: translation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Head from "next/head";
 import React from "react";
 import Header from "./Header";
 import Hero from "../../components/Hero";
+// import { useTranslation } from 'react-i18next';
 import Footer from "./Footer";
 
 const Home: NextPage = () => {
+	// const { t } = useTranslation();
+
 	return (
 		<div className='scrollbar-thin scrollbar-track-gray-700/20 scrollbar-thumb-red-900'>
 			<div className='gradient-01 h-screen text-gray-400 z-0 aboutt'>
